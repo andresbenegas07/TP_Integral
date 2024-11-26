c285611a299389dd0816086f307b73363784bafa
andresbenegas07 <andrescirobenegas@gmail.com>
2024-11-26 20:22:22 +0000

    ADD: lo dde hoy

--- Bash_script/alta_usuarios/alta_usuarios.sh
+++ Bash_script/alta_usuarios/alta_usuarios.sh
@@ -21,7 +21,10 @@ for LINEA in `cat $LISTA |  grep -v ^#`
 do
 	USUARIO=$(echo  $LINEA |awk -F ':' '{print $1}')
 	GRUPO=$(echo  $LINEA |awk -F ':' '{print $2}')
-	echo "sudo useradd -m -s /bin/bash -g $GRUPO $USUARIO"
+	DIR_HOME=$(echo $LINEA | awk -F ',' '{print $3}')
+
+	# Crear usuario con grupo y directorio home
+	echo "sudo useradd -m -s /bin/bash -g $GRUPO $USUARIO $DIR_HOME"
 done
 IFS=$ANT_IFS
 
